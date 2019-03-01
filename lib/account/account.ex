9a07e1afb13902e9a7f8ddbc37b75a8d1a802777
d-velev <dimitar.velev@quanterall.com>
2019-03-01 10:14:11 +0200

    Client specs and better error handling

--- lib/account/account.ex
+++ lib/account/account.ex
@@ -1,23 +1,28 @@
 defmodule AeppSDKElixir.Account.Account do
-
   alias AeppSDKElixir.Client.Worker, as: Client
 
   @type error :: {:error, String.t()}
 
-  @spec get_balance(String.t()) :: integer() | error()
+  @spec get_balance(String.t()) :: {:ok, integer()} | error()
   def get_balance(key) when is_binary(key) do
-    url = Client.get_url()
-    path = "#{url}/v2/accounts/#{key}"
+    case Client.get_url() do
+      {:ok, url} ->
+        path = "#{url}/v2/accounts/#{key}"
 
-    case Client.http_get(path) do
-      {:ok, %{"balance" => balance}} ->
-        {:ok, balance}
-      {:error, _} = error ->
-        error
+        case Client.http_get(path) do
+          {:ok, %{"balance" => balance}} ->
+            {:ok, balance}
+
+          {:error, _} = err ->
+            err
+        end
+
+      {:error, _} = err ->
+        err
     end
   end
 
-  def get_balance(key) do
-    {:error, "#{__MODULE__}: The key: #{key} is not a string"}
+  def get_balance(_) do
+    {:error, "#{__MODULE__}: Invalid pubkey type"}
   end
 end
--- lib/client/worker.ex
+++ lib/client/worker.ex
@@ -1,105 +1,134 @@
 defmodule AeppSDKElixir.Client.Worker do
-
   use GenServer
 
-  @spec start_link(any()) :: :ignore | {:error, any()} | {:ok, pid()}
+  alias __MODULE__, as: Client
+
+  defstruct [:url, :internal_url, :keypair, :network_id]
+
+  @type t :: %Client{
+          url: String.t(),
+          internal_url: String.t(),
+          keypair: keypair(),
+          network_id: String.t()
+        }
+  @type keypair :: %{pubkey: String.t(), privkey: String.t()}
+  @type error :: error()
+
   def start_link(_args) do
     GenServer.start_link(__MODULE__, {}, name: __MODULE__)
   end
 
   def init(_) do
-    {:ok, %{}}
+    {:ok, %Client{url: nil, internal_url: nil, keypair: nil, network_id: nil}}
   end
 
-  def configure(state) do
-    GenServer.call(__MODULE__, {:configure, state})
-  end
+  @spec configure(Client.t()) :: :ok | error()
+  def configure(
+        %Client{
+          url: url,
+          internal_url: internal_url,
+          keypair: %{pubkey: pubkey, privkey: privkey},
+          network_id: network_id
+        } = client
+      )
+      when is_binary(url) and is_binary(internal_url) and is_binary(pubkey) and is_binary(privkey) and
+             is_binary(network_id),
+      do: GenServer.call(__MODULE__, {:configure, client})
 
-  def get_url do
-    GenServer.call(__MODULE__, :get_url)
-  end
+  def configure(_), do: {:error, "Invalid client parameters"}
 
-  def get_internal_url do
-    GenServer.call(__MODULE__, :get_internal_url)
-  end
+  @spec get_url :: {:ok, String.t()}
+  def get_url, do: GenServer.call(__MODULE__, :get_url)
 
-  def get_keypair do
-    GenServer.call(__MODULE__, :get_keypair)
-  end
+  @spec get_internal_url :: {:ok, String.t()}
+  def get_internal_url, do: GenServer.call(__MODULE__, :get_internal_url)
 
-  def get_network_id do
-    GenServer.call(__MODULE__, :get_network_id)
-  end
+  @spec get_keypair :: {:ok, %{pubkey: String.t(), privkey: String.t()}}
+  def get_keypair, do: GenServer.call(__MODULE__, :get_keypair)
 
-  def set_url(url) do
-    GenServer.call(__MODULE__, {:set_url, url})
-  end
+  @spec get_network_id :: {:ok, String.t()}
+  def get_network_id, do: GenServer.call(__MODULE__, :get_network_id)
 
-  def set_internal_url(internal_url) do
-    GenServer.call(__MODULE__, {:set_internal_url, internal_url})
-  end
+  @spec set_url(String.t()) :: :ok | error()
+  def set_url(url) when is_binary(url), do: GenServer.call(__MODULE__, {:set_url, url})
 
-  def set_keypair(%{pubkey: _, privkey: _} = keypair) do
-    GenServer.call(__MODULE__, {:set_keypair, keypair})
-  end
+  def set_url(_), do: {:error, "Invalid URL type"}
 
-  def set_network_id(network_id) do
-    GenServer.call(__MODULE__, {:set_network_id, network_id})
-  end
+  @spec set_internal_url(String.t()) :: :ok | error()
+  def set_internal_url(internal_url) when is_binary(internal_url),
+    do: GenServer.call(__MODULE__, {:set_internal_url, internal_url})
 
-  def http_get(path) do
-    GenServer.call(__MODULE__, {:http_get, path})
-  end
+  def set_internal_url(_), do: {:error, "Invalid internal URL type"}
+
+  @spec set_keypair(keypair()) :: :ok | error()
+  def set_keypair(%{pubkey: pubkey, privkey: privkey} = keypair)
+      when is_binary(pubkey) and is_binary(privkey),
+      do: GenServer.call(__MODULE__, {:set_keypair, keypair})
+
+  def set_keypair(_), do: {:error, "Invalid keypair types or structure"}
+
+  @spec set_network_id(String.t()) :: :ok | error()
+  def set_network_id(network_id) when is_binary(network_id),
+    do: GenServer.call(__MODULE__, {:set_network_id, network_id})
+
+  def set_network_id(_), do: {:error, "Invalid network ID type"}
+
+  @spec http_get(String.t()) :: :ok | error()
+  def http_get(path) when is_binary(path), do: GenServer.call(__MODULE__, {:http_get, path})
+
+  def http_get(_), do: {:error, "Invalid path type"}
 
   def handle_call(
-        {:configure, %{url: _, internal_url: _, keypair: %{pubkey: _, privkey: _}, network_id: _} = config},
+        {:configure, client},
         _from,
         _state
-      ) do
-    {:reply, :ok, config}
-  end
+      ),
+      do: {:reply, :ok, client}
 
-  def handle_call(:get_url, _from, %{url: url} = state) do
-    {:reply, url, state}
-  end
+  def handle_call(:get_url, _from, %Client{url: nil} = state),
+    do: {:reply, {:error, "URL not configured"}, state}
 
-  def handle_call(:get_internal_url, _from, %{internal_url: internal_url} = state) do
-    {:reply, internal_url, state}
-  end
+  def handle_call(:get_url, _from, %Client{url: url} = state), do: {:reply, {:ok, url}, state}
 
-  def handle_call(:get_keypair, _from, %{keypair: keypair} = state) do
-    {:reply, keypair, state}
-  end
+  def handle_call(:get_internal_url, _from, %Client{internal_url: nil} = state),
+    do: {:reply, {:error, "Internal URL not configured"}, state}
 
-  def handle_call(:get_network_id, _from, %{network_id: network_id} = state) do
-    {:reply, network_id, state}
-  end
+  def handle_call(:get_internal_url, _from, %Client{internal_url: internal_url} = state),
+    do: {:reply, {:ok, internal_url}, state}
 
-  def handle_call({:set_url, url}, _from, state) do
-    {:reply, :ok, %{state | url: url}}
-  end
+  def handle_call(:get_keypair, _from, %Client{keypair: nil} = state),
+    do: {:reply, {:error, "Keypair not configured"}, state}
 
-  def handle_call({:set_internal_url, internal_url}, _from, state) do
-    {:reply, :ok, %{state | internal_url: internal_url}}
-  end
+  def handle_call(:get_keypair, _from, %Client{keypair: keypair} = state),
+    do: {:reply, {:ok, keypair}, state}
 
-  def handle_call({:set_keypair, keypair}, _from, state) do
-    {:reply, :ok, %{state | keypair: keypair}}
-  end
+  def handle_call(:get_network_id, _from, %Client{network_id: nil} = state),
+    do: {:reply, {:error, "Network ID not configured"}, state}
 
-  def handle_call({:set_network_id, network_id}, _from, state) do
-    {:reply, :ok, %{state | network_id: network_id}}
-  end
+  def handle_call(:get_network_id, _from, %Client{network_id: network_id} = state),
+    do: {:reply, {:ok, network_id}, state}
+
+  def handle_call({:set_url, url}, _from, state), do: {:reply, :ok, %Client{state | url: url}}
+
+  def handle_call({:set_internal_url, internal_url}, _from, state),
+    do: {:reply, :ok, %Client{state | internal_url: internal_url}}
+
+  def handle_call({:set_keypair, keypair}, _from, state),
+    do: {:reply, :ok, %Client{state | keypair: keypair}}
+
+  def handle_call({:set_network_id, network_id}, _from, state),
+    do: {:reply, :ok, %Client{state | network_id: network_id}}
 
   def handle_call({:http_get, path}, _from, state) do
     case HTTPoison.get(path) do
       {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
         {:reply, {:ok, Poison.decode!(body)}, state}
+
       {:ok, %HTTPoison.Response{body: body}} ->
         {:reply, {:error, Poison.decode!(body)["reason"]}, state}
+
       {:error, %HTTPoison.Error{reason: reason}} ->
         {:reply, {:error, reason}, state}
     end
   end
-
 end
