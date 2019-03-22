bf0b051dc76a037bd87db282fa7091c0af49a373
d-velev <dimitar.velev@quanterall.com>
2019-03-22 15:34:56 +0200

    Added coveralls and fixed save_keypair pattern match

--- apps/aeternity_node/mix.exs
+++ apps/aeternity_node/mix.exs
@@ -2,12 +2,15 @@ defmodule AeternityNode.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :aeternity_node,
-     version: "0.1.0",
-     elixir: "~> 1.8",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :aeternity_node,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
+    ]
   end
 
   # Configuration for the OTP application
--- apps/utils/mix.exs
+++ apps/utils/mix.exs
@@ -8,7 +8,8 @@ defmodule Utils.Mixfile do
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
