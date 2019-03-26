0d855f56eb526fa092a45a4e24886d824856b851
d-velev <dimitar.velev@quanterall.com>
2019-03-26 11:05:41 +0200

    Migrate doc to code, improve tests

--- apps/utils/lib/keys.ex
+++ apps/utils/lib/keys.ex
@@ -1,4 +1,7 @@
 defmodule Utils.Keys do
+  @moduledoc """
+  Key generation, handling, encoding and crypto
+  """
   alias Utils.Encoding
 
   @hex_base 16
@@ -12,19 +15,66 @@ defmodule Utils.Keys do
 
   @type password :: String.t()
 
+  @doc """
+  Generate a Curve25519 keypair
+
+  ## Examples
+      iex> Utils.Keys.generate_keypair()
+      %{
+        public: "ak_Q9DozPaq7fZ9WnB8SwNxuniaWwUmp1M7HsFTgCdJSsU2kKtC4",
+        secret: "227bdeedb4c3dd2b554ea6b448ac6788fbe66df1b4f87093a450bba748f296f5348bd07453735393e2ff8c03c65b4593f3bdd94f957a2e7cb314688b53441280"
+      }
+  """
+
   @spec generate_keypair :: keypair()
   def generate_keypair do
     %{public: binary_pubkey, secret: binary_privkey} = :enacl.sign_keypair()
     %{public: pubkey_from_binary(binary_pubkey), secret: privkey_from_binary(binary_privkey)}
   end
 
+  @doc """
+  Sign a binary message with the given private key
+
+  ## Examples
+      iex> message = "some message"
+      iex> privkey = <<34, 123, 222, 237, 180, 195, 221, 43, 85, 78, 166, 180, 72, 172, 103, 136,251, 230, 109, 241, 180, 248, 112, 147, 164, 80, 187, 167, 72, 242, 150, 245,52, 139, 208, 116, 83, 115, 83, 147, 226, 255, 140, 3, 198, 91, 69, 147, 243,189, 217, 79, 149, 122, 46, 124, 179, 20, 104, 139, 83, 68, 18, 128>>
+      iex> Utils.Keys.sign(message, privkey)
+      <<94, 26, 208, 168, 230, 154, 158, 226, 188, 217, 155, 170, 157, 33, 100, 22,247, 171, 91, 120, 249, 52, 147, 194, 188, 1, 14, 5, 15, 166, 232, 202, 97,96, 32, 32, 227, 151, 158, 216, 22, 68, 219, 5, 169, 229, 117, 147, 179, 43,...>>
+  """
+
   @spec sign(binary(), binary()) :: signature()
   def sign(message, privkey), do: :enacl.sign_detached(message, privkey)
 
+  @doc """
+  Verify that a message has been signed by a private key corresponding to the given public key
+
+  ## Examples
+      iex> signature = <<94, 26, 208, 168, 230, 154, 158, 226, 188, 217, 155, 170, 157, 33, 100, 22, 247, 171, 91, 120, 249, 52, 147, 194, 188, 1, 14, 5, 15, 166, 232, 202, 97, 96, 32, 32, 227, 151, 158, 216, 22, 68, 219, 5, 169, 229, 117, 147, 179, 43, 172, 211, 243, 171, 234, 254, 210, 119, 105, 248, 154, 19, 202, 7>>
+      iex> message = "some message"
+      iex> pubkey = <<52, 139, 208, 116, 83, 115, 83, 147, 226, 255, 140, 3, 198, 91, 69, 147, 243, 189, 217, 79, 149, 122, 46, 124, 179, 20, 104, 139, 83, 68, 18, 128>>
+      iex> Utils.Keys.verify(signature, message, pubkey)
+      {:ok, "some message"}
+  """
   @spec verify(message(), signature(), binary()) :: {:ok, message()} | {:error, atom()}
   def verify(signature, message, pubkey),
     do: :enacl.sign_verify_detached(signature, message, pubkey)
 
+  @doc """
+  Save a keypair at a given path with the specified file name. The keys are encrypted with the password and saved as separate files - `name` for the private and `{
+    name
+  }.pub` for the public key
+
+  ## Examples
+      keypair = Utils.Keys.generate_keypair
+      %{
+        public: "ak_2vTCdFVAvgkYUDiVpydmByybqSYZHEB189QcfjmdcxRef2W2eb",
+        secret: "f9cebe874d90626bfcea1093e72f22e500a92e95052b88aaebd5d30346132cb1fd1096207d3e887091e3c11a953c0238be2f9d737e2076bf89866bb786bc0fbf"
+      }
+      iex> password = "some password"
+      iex> path = "./keys"
+      iex> name = "key"
+      iex> Utils.Keys.save_keypair(keypair, password, path, name)
+  """
   @spec save_keypair(keypair(), password(), String.t(), String.t()) :: :ok | {:error, String.t()}
   def save_keypair(%{public: pubkey, secret: privkey}, password, path, name) do
     binary_pubkey = pubkey_to_binary(pubkey)
@@ -59,6 +109,20 @@ defmodule Utils.Keys do
     end
   end
 
+  @doc """
+  Attempt to read a keypair from a given path with the specified file name. If found, the keys will be decrypted with the password
+
+  ## Examples
+      iex> password = "some password"
+      iex> path = "./keys"
+      iex> name = "key"
+      iex> Utils.Keys.read_keypair(password, path, name)
+      {:ok,
+       %{
+         public: "ak_2vTCdFVAvgkYUDiVpydmByybqSYZHEB189QcfjmdcxRef2W2eb",
+         secret: "f9cebe874d90626bfcea1093e72f22e500a92e95052b88aaebd5d30346132cb1fd1096207d3e887091e3c11a953c0238be2f9d737e2076bf89866bb786bc0fbf"
+       }}
+  """
   @spec read_keypair(password(), String.t(), String.t()) ::
           {:ok, keypair()} | {:error, String.t()}
   def read_keypair(password, path, name) do
@@ -86,17 +150,52 @@ defmodule Utils.Keys do
     end
   end
 
+  @doc """
+  Convert a base58check public key string to binary
+
+  ## Examples
+      iex> pubkey = "ak_2vTCdFVAvgkYUDiVpydmByybqSYZHEB189QcfjmdcxRef2W2eb"
+      iex> Utils.Keys.pubkey_to_binary(pubkey)
+      <<253, 16, 150, 32, 125, 62, 136, 112, 145, 227, 193, 26, 149, 60, 2, 56, 190, 47, 157, 115, 126, 32, 118, 191, 137, 134, 107, 183, 134, 188, 15, 191>>
+      ```
+  """
+
   @spec pubkey_to_binary(pubkey()) :: binary()
-  def pubkey_to_binary(pubkey), do: Encoding.base58c_to_binary(pubkey)
+  def pubkey_to_binary(pubkey), do: Encoding.prefix_decode_base58c(pubkey)
 
+  @doc """
+  Convert a binary public key to a base58check string
+
+  ## Examples
+      iex> binary_pubkey = <<253, 16, 150, 32, 125, 62, 136, 112, 145, 227, 193, 26, 149, 60, 2, 56, 190, 47, 157, 115, 126, 32, 118, 191, 137, 134, 107, 183, 134, 188, 15, 191>>
+      iex> Utils.Keys.pubkey_from_binary(binary_pubkey)
+      "ak_2vTCdFVAvgkYUDiVpydmByybqSYZHEB189QcfjmdcxRef2W2eb"
+  """
   @spec pubkey_from_binary(binary()) :: pubkey()
-  def pubkey_from_binary(binary_pubkey), do: Encoding.binary_to_base58c("ak", binary_pubkey)
+  def pubkey_from_binary(binary_pubkey), do: Encoding.prefix_encode_base58c("ak", binary_pubkey)
 
+  @doc """
+  Convert a hex string private key to binary
+
+  ## Examples
+      iex> privkey = "f9cebe874d90626bfcea1093e72f22e500a92e95052b88aaebd5d30346132cb1fd1096207d3e887091e3c11a953c0238be2f9d737e2076bf89866bb786bc0fbf"
+      iex> Utils.Keys.privkey_to_binary(privkey)
+      <<249, 206, 190, 135, 77, 144, 98, 107, 252, 234, 16, 147, 231, 47, 34, 229, 0, 169, 46, 149, 5, 43, 136, 170, 235, 213, 211, 3, 70, 19, 44, 177, 253, 16, 150, 32, 125, 62, 136, 112, 145, 227, 193, 26, 149, 60, 2, 56, 190, 47, ...>>
+  """
   @spec privkey_to_binary(privkey()) :: binary()
   def privkey_to_binary(privkey) do
     {integer_privkey, _} = Integer.parse(privkey, @hex_base)
     :binary.encode_unsigned(integer_privkey)
   end
+
+  @doc """
+  Convert a binary private key to a hex string
+
+  ## Examples
+      iex> binary_privkey = <<249, 206, 190, 135, 77, 144, 98, 107, 252, 234, 16, 147, 231, 47, 34, 229, 0, 169, 46, 149, 5, 43, 136, 170, 235, 213, 211, 3, 70, 19, 44, 177, 253, 16, 150, 32, 125, 62, 136, 112, 145, 227, 193, 26, 149, 60, 2, 56, 190, 47, 157, 115, 126, 32, 118, 191, 137, 134, 107, 183, 134, 188, 15, 191>>
+      iex> Utils.Keys.privkey_from_binary(binary_privkey)
+      "f9cebe874d90626bfcea1093e72f22e500a92e95052b88aaebd5d30346132cb1fd1096207d3e887091e3c11a953c0238be2f9d737e2076bf89866bb786bc0fbf"
+  """
 
   @spec privkey_from_binary(binary()) :: privkey()
   def privkey_from_binary(binary_privkey) do
