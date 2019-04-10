1b177d52bab11c4e58904776e68e4a1c6bd066da
cheezus1 <bobinikolov33@gmail.com>
2019-04-10 15:27:51 +0300

    Fix compilation error

--- apps/core/mix.exs
+++ apps/core/mix.exs
@@ -28,10 +28,9 @@ defmodule Core.MixProject do
       {:aesophia,
        git: "https://github.com/aeternity/aesophia.git",
        manager: :rebar,
-       ref: "267fef3a5bb87c8ac35f125024fbfa07511f13de"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true},
+       ref: "267fef3a5bb87c8ac35f125024fbfa07511f13de"},
+    {:aeternity_node, in_umbrella: true},
+    {:utils, in_umbrella: true}
     ]
   end
 end
