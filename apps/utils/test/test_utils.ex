aa33be7c23a5a378da24ffa4b00d6667525a718d
Artur64 <artois641@gmail.com>
2019-05-02 15:34:35 +0300

    #38 Changed private and public keys

--- apps/utils/test/test_utils.ex
+++ apps/utils/test/test_utils.ex
@@ -289,16 +289,16 @@ defmodule TestUtils do
     client =
       Client.new(
         %{
-          public: "ak_2GSHayUGeqHXz2unJKpioHkXFXzjWBf3GhzVjQPdJunpBb4HT4",
+          public: "ak_6A2vcm1Sz6aqJezkLCssUXcyZTX7X8D5UwbuS2fRJr9KkYpRU",
           secret:
-            "42225194122641a843a160c92bf4b466213207299e4b6cbd3388ad31445b0d83a6beceb8f376deca8122846ad08f16212220eb9372ef6e6c28a7c93986a6ad3b"
+            "a7a695f999b1872acb13d5b63a830a8ee060ba688a478a08c6e65dfad8a01cd70bb4ed7927f97b51e1bcb5e1340d12335b2a2b12c8bc5221d63c4bcb39d41e61"
         },
         "ae_uat",
         "https://sdk-testnet.aepps.com/v2",
         "https://sdk-testnet.aepps.com/v2"
       )
 
-    valid_pub_key = "ak_2GSHayUGeqHXz2unJKpioHkXFXzjWBf3GhzVjQPdJunpBb4HT4"
+    valid_pub_key = "ak_6A2vcm1Sz6aqJezkLCssUXcyZTX7X8D5UwbuS2fRJr9KkYpRU"
     amount = 40_000_000
 
     [
