bb4b0af44c8cef19fab3c73b8bed4558e1ad76d1
Artur64 <artois641@gmail.com>
2019-07-24 17:37:25 +0300

    #95 Added new Channel GA+GA tests

--- test/core_channel_test.exs
+++ test/core_channel_test.exs
@@ -1,7 +1,7 @@
 defmodule CoreChannelTest do
   use ExUnit.Case
 
-  alias Core.{Account, Client, Channel}
+  alias Core.{Account, Client, Channel, GeneralizedAccount}
   alias Utils.{Encoding, Transaction}
 
   setup_all do
@@ -29,7 +29,31 @@ defmodule CoreChannelTest do
         "http://localhost:3113/v2"
       )
 
-    [client: client, client1: client1]
+    client2 =
+      Client.new(
+        %{
+          public: "ak_fvRLMDGGFUSNbQcokMyM3RmagHBFX2vmcaSAnB7UTkwvKnb9G",
+          secret:
+            "49cb5ce722e59df4c4ccc99ff168653c9f08a09dc1ffebbc48956c3bb2474d52585ffcdfb2f44a42e1cca0879fcbc2c3562a0bf04d50686d80050579ff204935"
+        },
+        "my_test",
+        "http://localhost:3013/v2",
+        "http://localhost:3113/v2"
+      )
+
+    client3 =
+      Client.new(
+        %{
+          public: "ak_mexPLgurwPYLKgLBM885hwiobHnv4c12s5Gi3UuSPZDhYLrmx",
+          secret:
+            "cc86e9d5127fac3225b00c8c1ac552b31b64c4ed9545e36240aa54a7988f72256564ad224800dec63ae4f48cf23486a43862af660d7877d953027ddaccd68327"
+        },
+        "my_test",
+        "http://localhost:3013/v2",
+        "http://localhost:3113/v2"
+      )
+
+    [client: client, client1: client1, client2: client2, client3: client3]
   end
 
   @tag :travis_test
@@ -108,5 +132,113 @@ defmodule CoreChannelTest do
              {:ok, %{reason: "Channel not found"}},
              Channel.get_by_pubkey(setup_data.client, channel_id)
            )
+  end
+
+  @tag :travis_test
+  test "Channel workflow with generalized accounts", setup_data do
+    %{client: client, client2: client2, client3: client3} = setup_data
+    source_code = "contract Authorization =
+
+    function auth(auth_value : bool) =
+      auth_value"
+    auth = [auth_contract_source: source_code, auth_args: ["true"], fee: 100_000_000_000]
+
+    assert match?(
+             {:ok, _},
+             Account.spend(
+               client,
+               client2.keypair.public,
+               1_000_000_000_000_000_000
+             )
+           )
+
+    assert match?(
+             {:ok, _},
+             Account.spend(
+               client,
+               client3.keypair.public,
+               1_000_000_000_000_000_000
+             )
+           )
+
+    GeneralizedAccount.attach(client2, source_code, "auth", [])
+    GeneralizedAccount.attach(client3, source_code, "auth", [])
+
+    assert {:ok, [tx, create_meta_tx, []]} =
+             Core.Channel.create(
+               client2,
+               1000,
+               client3.keypair.public,
+               1000,
+               1000,
+               1000,
+               100,
+               Utils.Encoding.prefix_encode_base58c("st", <<0::256>>),
+               auth: auth
+             )
+
+    assert {:ok, [^tx, create_meta_tx1, []]} = Utils.Transaction.sign_tx(tx, client3, auth)
+
+    assert {:ok, %{channel_id: channel_id}} =
+             Core.Channel.post(client2, create_meta_tx, inner_tx: create_meta_tx1, tx: tx)
+
+    assert {:ok, %{id: ^channel_id}} = Core.Channel.get_by_pubkey(client2, channel_id)
+
+    {:ok, %{round: round}} = Core.Channel.get_by_pubkey(client2, channel_id)
+    next_round = round + 1
+
+    assert {:ok, [deposit_tx, deposit_meta_tx, deposit_sig]} =
+             Core.Channel.deposit(
+               client2,
+               100_000_000_000,
+               channel_id,
+               next_round,
+               Utils.Encoding.prefix_encode_base58c("st", <<0::256>>),
+               auth: auth
+             )
+
+    assert {:ok, [^deposit_tx, deposit_meta_tx1, deposit_sig1]} =
+             Utils.Transaction.sign_tx(deposit_tx, client3, auth)
+
+    assert {:ok, _} =
+             Core.Channel.post(client2, deposit_meta_tx,
+               inner_tx: deposit_meta_tx1,
+               tx: deposit_tx
+             )
+
+    {:ok, %{round: round}} = Core.Channel.get_by_pubkey(client2, channel_id)
+    next_round = round + 1
+
+    assert {:ok, [withdraw_tx, withdraw_meta_tx, withdraw_sig]} =
+             Core.Channel.withdraw(
+               client2,
+               channel_id,
+               client2.keypair.public,
+               2000,
+               Utils.Encoding.prefix_encode_base58c("st", <<0::256>>),
+               next_round,
+               auth: auth
+             )
+
+    assert {:ok, [^withdraw_tx, withdraw_meta_tx1, withdraw_sig1]} =
+             Utils.Transaction.sign_tx(withdraw_tx, client3, auth)
+
+    assert {:ok, _} =
+             Core.Channel.post(client2, withdraw_meta_tx,
+               inner_tx: withdraw_meta_tx1,
+               tx: withdraw_tx
+             )
+
+    assert {:ok, [close_mutual_tx, close_mutual_meta_tx, close_mutual_sig]} =
+             Core.Channel.close_mutual(client2, channel_id, 1000, 1000, auth: auth)
+
+    assert {:ok, [^close_mutual_tx, close_mutual_meta_tx1, close_mutual_sig1]} =
+             Utils.Transaction.sign_tx(close_mutual_tx, client3, auth)
+
+    assert {:ok, _} =
+             Core.Channel.post(client2, close_mutual_meta_tx,
+               inner_tx: close_mutual_meta_tx1,
+               tx: close_mutual_tx
+             )
   end
 end
