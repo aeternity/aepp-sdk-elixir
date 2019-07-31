25751f1eefe9430672f6e0ad9379106fe405d6c4
danielaivanova <danielaivanova_@abv.bg>
2019-07-31 12:17:32 +0300

    #112 Test commit

--- lib/core/client.ex
+++ lib/core/client.ex
@@ -76,7 +76,9 @@ defmodule Core.Client do
       when is_binary(public_key) and is_binary(secret_key) and is_binary(network_id) and
              is_binary(url) and is_binary(internal_url) do
     connection = Connection.new(url)
+    connection = %{connection | adapter: [recv_timeout: 30_000]}
     internal_connection = Connection.new(internal_url)
+    internal_connection = %{internal_connection | adapter: [recv_timeout: 30_000]}
 
     %Client{
       keypair: keypair,
